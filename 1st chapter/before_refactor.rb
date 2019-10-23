0f5a015240d032621e386d7dc31c69128d89472e
Toni Dezman <toni.dezman@gmail.com>
2019-10-24 01:28:39 +0200

    from camelcase to snake case

--- 1st chapter/before_refactor.rb
+++ 1st chapter/before_refactor.rb
@@ -9,40 +9,40 @@ def format(money)
 end
 
 def statement(invoice, plays)
-  totalAmount = 0
-  volumeCredits = 0
+  total_amount = 0
+  volume_credits = 0
   result = "Statement for #{invoice['customer']}\n"
 
   invoice['performances'].each do |perf|
     play = plays[perf['playID']]
-    thisAmount = 0
+    this_amount = 0
 
     case play['type']
     when 'tragedy'
-      thisAmount = 40_000
-      thisAmount += 1_000 * (perf['audience'] - 30) if perf['audience'] > 30
+      this_amount = 40_000
+      this_amount += 1_000 * (perf['audience'] - 30) if perf['audience'] > 30
     when 'comedy'
-      thisAmount = 30_000
+      this_amount = 30_000
       if perf['audience'] > 20
-        thisAmount += 10_000 + 500 * (perf['audience'] - 20)
+        this_amount += 10_000 + 500 * (perf['audience'] - 20)
       end
-      thisAmount += 300 * perf['audience']
+      this_amount += 300 * perf['audience']
     else
       raise "unknown type: #{play['type']}"
     end
 
     # add volume credits
-    volumeCredits += [perf['audience'] - 30, 0].max
+    volume_credits += [perf['audience'] - 30, 0].max
     # add extra credit for every ten comedy attendees
-    volumeCredits += (perf['audience'] / 5.0).floor if 'comedy' == play['type']
+    volume_credits += (perf['audience'] / 5.0).floor if 'comedy' == play['type']
 
     # print line for this order
     result +=
-      "  #{play['name']}: #{format(thisAmount / 100.0)} (#{perf['audience']} seats)\n"
-    totalAmount += thisAmount
+      "  #{play['name']}: #{format(this_amount / 100.0)} (#{perf['audience']} seats)\n"
+    total_amount += this_amount
   end
-  result += "Amount owed is #{format(totalAmount / 100.0)}\n"
-  result += "You earned #{volumeCredits} credits\n"
+  result += "Amount owed is #{format(total_amount / 100.0)}\n"
+  result += "You earned #{volume_credits} credits\n"
   result
 end
 
