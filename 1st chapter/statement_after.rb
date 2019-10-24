02df915c203d5bb8a0c347383cfdb6f2f6a88423
Toni Dezman <toni.dezman@gmail.com>
2019-10-24 02:17:08 +0200

    small refactorings

--- 1st chapter/statement_after.rb
+++ 1st chapter/statement_after.rb
@@ -2,20 +2,21 @@ def format(money)
   "$#{sprintf('%.2f', money)}"
 end
 
-def amount_for(perf, play)
+def amount_for(performance, play)
   case play['type']
   when 'tragedy'
     this_amount = 40_000
-    this_amount += 1_000 * (perf['audience'] - 30) if perf['audience'] > 30
+    this_amount += 1_000 * (performance['audience'] - 30) if performance['audience'] > 30
   when 'comedy'
     this_amount = 30_000
-    if perf['audience'] > 20
-      this_amount += 10_000 + 500 * (perf['audience'] - 20)
+    if performance['audience'] > 20
+      this_amount += 10_000 + 500 * (performance['audience'] - 20)
     end
-    this_amount += 300 * perf['audience']
+    this_amount += 300 * performance['audience']
   else
     raise "unknown type: #{play['type']}"
   end
+  this_amount
 end
 
 def statement(invoice, plays)
@@ -23,21 +24,20 @@ def statement(invoice, plays)
   volume_credits = 0
   result = "Statement for #{invoice['customer']}\n"
 
-  invoice['performances'].each do |perf|
-    play = plays[perf['playID']]
-    this_amount = amount_for(perf, play)
+  invoice['performances'].each do |performance|
+    play = plays[performance['playID']]
 
     # add volume credits
-    volume_credits += [perf['audience'] - 30, 0].max
+    volume_credits += [performance['audience'] - 30, 0].max
     # add extra credit for every ten comedy attendees
-    volume_credits += (perf['audience'] / 5.0).floor if 'comedy' == play['type']
+    volume_credits += (performance['audience'] / 5.0).floor if 'comedy' == play['type']
 
     # print line for this order
     result +=
-      "  #{play['name']}: #{format(this_amount / 100.0)} (#{
-        perf['audience']
+      "  #{play['name']}: #{format(amount_for(performance, play) / 100.0)} (#{
+        performance['audience']
       } seats)\n"
-    total_amount += this_amount
+    total_amount += amount_for(performance, play)
   end
   result += "Amount owed is #{format(total_amount / 100.0)}\n"
   result += "You earned #{volume_credits} credits\n"
